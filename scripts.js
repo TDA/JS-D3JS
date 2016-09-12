58c5c72db1290de82b3e0150f089e53ee0eacf39
Sai Chandramouli <schand31@asu.edu>
2016-09-11 17:25:04 -0700

    some changes

--- scripts.js
+++ scripts.js
@@ -2,7 +2,8 @@
  * Created by schandramouli on 12/28/15.
  */
 
-d3.select("body").append("svg").attr("width", 100).attr("height", 100)
+d3.select("body")
+  .append("svg").attr({"width": 100, "height": 100})
   .append("circle").attr("cx", 25).attr("cy", 25).attr("r", 25).style("fill", "purple");
 
 // looks like obj literals work, yay!
@@ -11,15 +12,21 @@ d3.select("svg").append("circle").attr({
   "cy": 40,
   "r": 30
 }).style({
-  "fill": "blue"
+  "fill": "rgba(0, 0, 0, 0.8)"
 });
 
 var theData = [1, 2, 3];
 
+var theData2 = {
+  1:4,
+  2:5,
+  3:6
+};
+
 var p = d3.select("body").selectAll("p")
-  .data(theData)
+  .data(d3.keys(theData2)) // imaginary element binds to an array
   .enter()
-  .append("p")
-  .text(function (d,i,f,g) {
-    return "i = " + i + " d = "+d + " f= " +f+ " g= " + g;
-  });
+    .append("p")
+      .text(function (data, index) {
+        return "index = " + index + " data = "+ data + " value " + theData2[data];
+      });
